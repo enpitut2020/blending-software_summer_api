495d115dfb9dfcfdbbf8d414327192ec745132dd
name <email>
2020-07-24 14:59:08 +0900

    ネットワーク分析組み込み

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import requests
+import networkx as nx
 
 app = Flask(__name__)
 
@@ -11,14 +13,22 @@ def post():
         response = requests.get(url)
         return response
 
-@app.route(f'/personalized_pagerank/<ch_id>', methods=['GET'])
+@app.route(f'/personalized_pagerank/<ch_id>', methods=['POST'])
 def personalized_pagerank(ch_id=None):
     """
     現段階では本当のページランクではなく、推薦するチャンネルのIDが保存されたjsonを返す。
     チャンネルIDは以下のようにURLに含まれる
     youtube.com/user/チャンネルID  (チャンネルのホームページのURL)
     """
-    ids_of_recommended_channel = {"ch_id": ["jpspygea", "YamatoNjp", "Poulmt", "vodkaplaysful"]}
+    ch_id = request.form['channel_id']
+    # グラフ作成
+    G = nx.read_edgelist("data/edge_list.txt", delimiter=' , ')
+    # pagerank計算
+    pr = nx.pagerank(G, personalization={ch_id: 1})
+    # 自分は除外
+    del pr[ch_id]
+    # ソートしてjsonにエンコード
+    ids_of_recommended_channel = dict(sorted(pr.items(), key=lambda x: -x[1])[:5])
     return jsonify(ids_of_recommended_channel)
 
 if __name__ == '__main__':
